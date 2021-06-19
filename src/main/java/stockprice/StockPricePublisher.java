a06abbf82f18997d709ba52317df7ed00c52cd21
buntychakraborty <buntychakraborty@outlook.com>
2021-06-19 19:11:17 +0530

    stock-price added

---
+++ src/main/java/stockprice/StockPricePublisher.java
@@ -0,0 +1,21 @@
+package stockprice;
+
+import java.time.Duration;
+import java.util.concurrent.atomic.AtomicInteger;
+
+import com.github.javafaker.Faker;
+
+import reactor.core.publisher.Flux;
+
+public class StockPricePublisher {
+
+    public static Flux<Integer> getPrice(){
+        AtomicInteger atomicInteger = new AtomicInteger(100);
+        return Flux.interval(Duration.ofSeconds(1))
+                    .map(i -> atomicInteger.getAndAccumulate(
+                          new Faker().random().nextInt(-5, 5),
+                            Integer::sum
+                    ));
+    }
+
+}
